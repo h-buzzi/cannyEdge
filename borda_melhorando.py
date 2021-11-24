b5d4a8b0216b90f1ee0254e79ee52da5546c6265
Henrique Eissmann Buzzi <71103223+h-buzzi@users.noreply.github.com>
2021-11-24 00:10:25 -0300

    Developed but needs formatting

--- borda_melhorando.py
+++ borda_melhorando.py
@@ -6,14 +6,14 @@ Created on Tue Nov 23 08:21:06 2021
 """
 
 import cv2
-import sys
 import numpy as np
-from matplotlib import pyplot as pp
+import time
 
 ###### INPUTS
-I = cv2.imread('castle.jpg', cv2.IMREAD_GRAYSCALE)
+start = time.time()
+I = np.float32(cv2.imread('castle.jpg', cv2.IMREAD_GRAYSCALE))
 dp = 2 # Sigma
-l_Sup = 0.5
+l_Sup = 0.45
 l_Inf = 0.1
 
 ###### Suaviza imagem por GaussianBlur
@@ -26,15 +26,13 @@ K_l = K_c.T
 I_c = cv2.filter2D(src = Is, ddepth = -1, kernel = K_c)
 I_l = cv2.filter2D(src = Is, ddepth = -1, kernel = K_l)
 
-M = np.sqrt(I_c*I_c + I_l*I_l)
-cv2.imshow('M', np.uint8(255*M/np.amax(M)))
-cv2.waitKey(0)
+M = np.sqrt((I_c*I_c) + (I_l*I_l))
 P = np.arctan2(I_c,I_l)*180/np.pi
 P[P<0]=P[P<0]+180
 G_N = np.zeros(I.shape)
 
 ## 1
-ind1 = P<22.5
+ind1 = (P<22.5) + (P>=157.5) 
 ind2 = np.roll(ind1,-1,axis=1)
 ind3 = np.roll(ind1,1,axis=1)
 ind4 = (M*ind1 > M*ind2) == (M*ind1 > M*ind2)
@@ -54,5 +52,19 @@ ind3 = np.roll(ind1,1,axis=0)
 ind4 = (M*ind1 > M*ind2) == (M*ind1 > M*ind2)
 G_N[ind4] = M[ind4]
 
-##
+## 4
+ind1 = (P>=112.5)*(P<157.5)
+ind2 = np.roll(ind1,(1,-1),axis=(1,0))
+ind3 = np.roll(ind1,(1,-1),axis=(0,1))
+ind4 = (M*ind1 > M*ind2) == (M*ind1 > M*ind2)
+G_N[ind4] = M[ind4]
 
+##Imagens binárias
+G_NH = 255*np.ones(I.shape,dtype=np.uint8)*(G_N>(l_Sup*np.amax(G_N)))
+G_NL = 255*np.ones(I.shape,dtype=np.uint8)*(G_N>l_Inf*np.amax(G_N))
+print(time.time()-start)
+cv2.imwrite('D:\Trabalho\Python\cannyEdge-main\saida.jpg',G_NH)
+cv2.imshow('G_NH', G_NH)
+cv2.waitKey(0)
+cv2.imshow('G_NL', G_NL)
+cv2.waitKey(0)
